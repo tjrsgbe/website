e2a464a3370a564f2652a899a91958566404632f
tjrsgbe <64323579+tjrsgbe@users.noreply.github.com>
2025-02-11 10:49:23 +0100

    Update solucao.sh

--- solucao.sh
+++ solucao.sh
@@ -9,8 +9,6 @@ expect "2. Quem são as principais partes interessadas envolvidas? \r"
 send -- "Todos os seres, particularmente o humano por ter mais poder e logo mais responsabilidade de agir tendo em conta o fluir da informática existente. \r"
 expect "3. Quais são as dinâmicas de poder em jogo? \r"
 send -- "São as dinâmicas algorítmicas que dão oportunidade de ir mais além. \r"
-expect "3. Quais são as dinâmicas de poder em jogo? \r"
-send -- "São as dinâmicas algorítmicas que dão oportunidade de ir mais além. \r"
 expect "4. Como é que esta solução afeta os diferentes grupos da sociedade? \r"
 send -- "Na medida em que qualquer pessoa pode contribuir podendo unir estes. \r"
 expect eof
